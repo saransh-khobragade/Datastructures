29fec1f8450b89de75d6a214ab6d8ca6b08b6424
saransh-khobragade <saransh98@gmail.com>
2025-08-26 12:04:27 +0530

    5 ms rotate array.

---
+++ python/patterns/two-pointer/189. Rotate Array.py
@@ -0,0 +1,44 @@
+"""
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+"""
+
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k = k % len(nums)
+        if k == 0:
+            return
+
+        if k > len(nums) - 1:
+            print(nums)
+            return
+        start = 0
+        end = len(nums) - 1
+
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+        start = 0
+        end = k - 1
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+        start = k
+        end = len(nums) - 1
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+        # print(nums)
+
+
+s = Solution()
+s.rotate([1, 2, 3, 4, 5, 6], 2)
