0905bb43b23c249e18f7d5a709fbd3994707867c
saransh-khobragade <saransh98@gmail.com>
2025-09-05 11:45:55 +0530

    lets go

--- patterns/sliding-window/643. Maximum Average Subarray I/643. Maximum Average Subarray I.js
+++ patterns/sliding-window/643. Maximum Average Subarray I/643. Maximum Average Subarray I.js
@@ -1,13 +1,12 @@
-// 643. Maximum Average Subarray I
-// LeetCode Problem: https://leetcode.com/problems/maximum-average-subarray-i/
+/**
+ * You are given an integer array nums consisting of n elements, and an integer k.
+ * Find a contiguous subarray whose length is equal to k that has the maximum average value 
+ * and return this value.
+ */
 
 class Solution {
     findMaxAverage(nums, k) {
-        /**
-         * You are given an integer array nums consisting of n elements, and an integer k.
-         * Find a contiguous subarray whose length is equal to k that has the maximum average value 
-         * and return this value.
-         */
+        
         let start = 0;
         let end = 0;
         let currSum = 0;
