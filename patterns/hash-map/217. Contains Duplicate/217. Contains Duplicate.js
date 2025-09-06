9e1fbf2a3b60a68ade3948493e21128a717c054d
saransh-khobragade <saransh98@gmail.com>
2025-09-06 15:37:04 +0530

    fixed anagram

--- patterns/hash-map/217. Contains Duplicate/217. Contains Duplicate.js
+++ patterns/hash-map/217. Contains Duplicate/217. Contains Duplicate.js
@@ -1,12 +1,10 @@
-// 217. Contains Duplicate
-// LeetCode Problem: https://leetcode.com/problems/contains-duplicate/
+/**
+ * Given an integer array nums, return true if any value appears at least twice
+ * in the array, and return false if every element is distinct.
+ */
 
 class Solution {
     containsDuplicate(nums) {
-        /**
-         * Given an integer array nums, return true if any value appears at least twice 
-         * in the array, and return false if every element is distinct.
-         */
         const frequency = new Set();
         for (let x of nums) {
             if (frequency.has(x)) {
--- patterns/hash-map/242. Valid Anagram/242. Valid Anagram.js
+++ patterns/hash-map/242. Valid Anagram/242. Valid Anagram.js
@@ -1,34 +1,34 @@
-// 242. Valid Anagram
-// LeetCode Problem: https://leetcode.com/problems/valid-anagram/
+/**
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different
+ * word or phrase, typically using all the original letters exactly once.
+ */
+var isAnagram = function (s, t) {
+    if (s.length != t.length) return false;
 
-class Solution {
-    isAnagram(s, t) {
-        /**
-         * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-         * 
-         * An Anagram is a word or phrase formed by rearranging the letters of a different 
-         * word or phrase, typically using all the original letters exactly once.
-         */
-        s = s.split('');
-        t = t.split('');
-
-        if (s.length === t.length) {
-            for (let x of s) {
-                if (!t.includes(x)) {
-                    return false;
-                } else {
-                    t.splice(t.indexOf(x), 1);
-                }
-            }
-            return true;
+    hmap1 = {};
+    hmap2 = {};
+    for (let i = 0; i < s.length; i++) {
+        if (hmap1[s[i]]) {
+            hmap1[s[i]] = hmap1[s[i]] + 1;
         } else {
+            hmap1[s[i]] = 1;
+        }
+        if (hmap2[t[i]]) {
+            hmap2[t[i]] = hmap2[t[i]] + 1;
+        } else {
+            hmap2[t[i]] = 1;
+        }
+    }
+    for (x in hmap1) {
+        if (hmap1[x] !== hmap2[x]) {
             return false;
         }
     }
-}
-
-const s = new Solution();
-console.log(s.isAnagram("aacc", "ccac")); // false
+    return true;
+};
+console.log(isAnagram("aacc", "ccac")); // false
 
 /*
 Time complexity: O(N)
