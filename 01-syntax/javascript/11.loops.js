71fef040653ebb29d5e3dc8c0106e0bb6aa5c821
saransh-khobragade <saransh98@gmail.com>
2025-09-06 14:32:36 +0530

    1493 done

--- 01-syntax/javascript/11.loops.js
+++ 01-syntax/javascript/11.loops.js
@@ -6,66 +6,55 @@ while (i < 3) {
     console.log(i);
     i++;
 }
-// 0
-// 1
-// 2
 
 // For loop for elements
 const fruits = ["apple", "banana", "cherry"];
 for (const x of fruits) {
     console.log("first " + x);
 }
-// first apple
-// first banana
-// first cherry
 
+// For loop for index
+const fruits2 = ["apple", "banana", "cherry"];
+for (const i in fruits) {
+    console.log("first " + x);
+}
 // For loop for index
 for (let j = 0; j < fruits.length; j++) {
     console.log(j);
 }
-// 0
-// 1
-// 2
+
+// For loop for index and element
+for (let [i, v] of fruits.entries()) {
+    console.log(i, v);
+}
 
 // For loop for index and element
 fruits.forEach((v, k) => {
     console.log(k, v);
 });
-// 0 apple
-// 1 banana
-// 2 cherry
 
 // For loop for range
 for (let l = 0; l < 2; l++) {
     console.log(l);
 }
-// 0
-// 1
 
 // For loop for range
 for (let m = 2; m < 4; m++) {
     console.log("third " + m);
 }
-// third 2
-// third 3
 
 // For loop for range with step
 for (let n = 2; n < 6; n += 2) {
     console.log("fourth " + n);
 }
-// fourth 2
-// fourth 4
 
 // For loop in dictionary/object
 const d = {
     name: "John",
     age: "30",
-    city: "New York"
+    city: "New York",
 };
 
 for (const [key, value] of Object.entries(d)) {
     console.log(key, value);
 }
-// name John
-// age 30
-// city New York 
---
+++ patterns/sliding-window/1493. Longest Subarray of 1's After Deleting One Element/1493. Longest Subarray of 1's After Deleting One Element.js
@@ -0,0 +1,28 @@
+/**
+Given a binary array nums, you should delete one element from it.
+Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+Input: nums = [1,1,0,1]
+Output: 3
+Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.*/
+const longestSubarray = function (nums) {
+    let left = 0;
+    let right = 0;
+    let zeros = 0;
+    let maxCount = 0;
+
+    while (right < nums.length) {
+        if (nums[right] == 0) {
+            zeros += 1;
+        }
+        while (zeros > 1) {
+            if (nums[left] == 0) {
+                zeros -= 1;
+            }
+            left += 1;
+        }
+        right += 1;
+        maxCount = Math.max(maxCount, right - left);
+    }
+    return maxCount - 1;
+};
+console.log(longestSubarray([0, 1, 1, 1, 0, 0, 1, 1, 0, 1]));
