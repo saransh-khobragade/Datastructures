b7d4b569bf9c73ce921751f4f21190f116cd5f07
saransh-khobragade <saransh98@gmail.com>
2025-09-07 11:25:41 +0530

    longest substring without repeating done

---
+++ patterns/sliding-window/3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters.js
@@ -0,0 +1,21 @@
+/*
+Given a string s, find the length of the longest substring without duplicate characters.
+*/
+var lengthOfLongestSubstring = function (s) {
+    let start = 0;
+    let end = 0;
+    let seen = new Set();
+    let count = 0;
+    while (end < s.length) {
+        if (seen.has(s[end])) {
+            seen.delete(s[start]);
+            start += 1;
+        } else {
+            seen.add(s[end]);
+            end += 1;
+        }
+        count = Math.max(count, end - start);
+    }
+    return count;
+};
+console.log(lengthOfLongestSubstring("abcabcbb"));
