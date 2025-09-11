6df0a85ba94e3693621660e3d5a72a788573d895
saransh-khobragade <saransh98@gmail.com>
2025-09-11 22:11:54 +0530

    Done

--- patterns/arrays/1431. Kids With the Greatest Number of Candies/1431. Kids With the Greatest Number of Candies.js
+++ patterns/arrays/1431. Kids With the Greatest Number of Candies/1431. Kids With the Greatest Number of Candies.js
@@ -1,21 +1,18 @@
-// 1431. Kids With the Greatest Number of Candies
-// LeetCode Problem: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
-
+/**
+ * Given the array candies and the integer extraCandies, where candies[i] represents
+ * the number of candies that the ith kid has.
+ *
+ * For each kid, check if there is a way to distribute extraCandies among the kids
+ * such that he or she can have the greatest number of candies among them.
+ *
+ * Return a boolean array result of length n, where result[i] is true if, after giving
+ * the ith kid all the extraCandies, they will have the greatest number of candies
+ * among all the kids, or false otherwise.
+ */
 function kidsWithCandies(candies, extraCandies) {
-    /**
-     * Given the array candies and the integer extraCandies, where candies[i] represents 
-     * the number of candies that the ith kid has.
-     * 
-     * For each kid, check if there is a way to distribute extraCandies among the kids 
-     * such that he or she can have the greatest number of candies among them.
-     * 
-     * Return a boolean array result of length n, where result[i] is true if, after giving 
-     * the ith kid all the extraCandies, they will have the greatest number of candies 
-     * among all the kids, or false otherwise.
-     */
     const maxCandies = Math.max(...candies);
     const result = [];
-    
+
     for (let candy of candies) {
         if (candy + extraCandies >= maxCandies) {
             result.push(true);
@@ -23,7 +20,7 @@ function kidsWithCandies(candies, extraCandies) {
             result.push(false);
         }
     }
-    
+
     return result;
 }
 
--- patterns/prefix-sum/1732. Find the Highest Altitude/1732. Find the Highest Altitude.js
+++ patterns/prefix-sum/1732. Find the Highest Altitude/1732. Find the Highest Altitude.js
@@ -1,15 +1,13 @@
-// 1732. Find the Highest Altitude
-// LeetCode Problem: https://leetcode.com/problems/find-the-highest-altitude/
+/**
+ * There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes.
+ * The biker starts his trip on point 0 with altitude equal 0.
+ *
+ * You are given an integer array gain of length n where gain[i] is the net gain in altitude
+ * between points i and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+ */
 
 class Solution {
     largestAltitude(gain) {
-        /**
-         * There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. 
-         * The biker starts his trip on point 0 with altitude equal 0.
-         * 
-         * You are given an integer array gain of length n where gain[i] is the net gain in altitude 
-         * between points i and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
-         */
         let s = 0;
         let maxSum = 0; // Start at 0 since biker begins at altitude 0
         for (let x of gain) {
