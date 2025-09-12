570ad2b3e218069a6c2697818c80ca26f2df0f77
saransh-khobragade <saransh98@gmail.com>
2025-09-12 15:53:59 +0530

    updated python

--- patterns/two-pointer/238. Product of Array Except Self/238. Product of Array Except Self.py
+++ patterns/two-pointer/238. Product of Array Except Self/238. Product of Array Except Self.py
@@ -5,29 +5,32 @@ You must write an algorithm that runs in O(n) time and without using the divisio
 Input: nums = [1,2,3,4]
 Output: [24,12,8,6]
 """
+
 from typing import List
+
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        leftArr = [1]*len(nums)
-        rightArr =  [1]*len(nums)
+        n = len(nums)
+        res = [1] * n
 
-        left = 1
-        for i in range(0,len(nums)-1):
-            leftArr[i] = nums[i] * left
-            left *= nums[i]
-        
-        right = 1
-        
-        for i in range(len(nums)-1,-1,-1):
-            rightArr[i] = leftArr[i - 1] * right
-            right *= nums[i]
+        # prefix pass
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
 
-        return rightArr
-        
+        # suffix pass
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
+
+        return res
 
 
 s = Solution()
-s.productExceptSelf([1,2,3,4])
+s.productExceptSelf([1, 2, 3, 4])
 
 # 1     2   6   1  left products
 # 1     2   3   4
