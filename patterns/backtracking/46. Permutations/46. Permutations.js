8d89eb27eec6f6287a5b87bc42bf47cf5e8cda00
saransh-khobragade <saransh98@gmail.com>
2025-09-18 18:09:15 +0530

    renaming

--- patterns/backtracking/46. Permutations/46. Permutations.js
+++ patterns/backtracking/46. Permutations/46. Permutations.js
@@ -1,19 +1,17 @@
-// 46. Permutations
-// LeetCode Problem: https://leetcode.com/problems/permutations/
+/**
+ * Given an array nums of distinct integers, return all the possible permutations.
+ * You can return the answer in any order.
+ */
 
 function permute(nums) {
-    /**
-     * Given an array nums of distinct integers, return all the possible permutations. 
-     * You can return the answer in any order.
-     */
     const result = [];
-    
+
     function backtrack(currentPermutation) {
         if (currentPermutation.length === nums.length) {
             result.push([...currentPermutation]);
             return;
         }
-        
+
         for (let num of nums) {
             if (!currentPermutation.includes(num)) {
                 currentPermutation.push(num);
@@ -22,7 +20,7 @@ function permute(nums) {
             }
         }
     }
-    
+
     backtrack([]);
     return result;
 }
