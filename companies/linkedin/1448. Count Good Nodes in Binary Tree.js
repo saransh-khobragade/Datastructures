6a08074b167a95532dc54dff1d646755446d1c4f
saransh-khobragade <saransh98@gmail.com>
2025-09-24 23:39:23 +0530

    done

--- companies/linkedin/1448. Count Good Nodes in Binary Tree.js
+++ companies/linkedin/1448. Count Good Nodes in Binary Tree.js
@@ -5,4 +5,55 @@ Input: root = [3,1,4,3,null,1,5]
 Output: 4
 Explanation: Nodes in blue are good.
 */
-var goodNodes = function (root) {};
+class TreeNode {
+    constructor(val, left, right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class BinaryTree {
+    buildByLevelOrder(levelArr) {
+        let start = new TreeNode(levelArr[0], null, null);
+        let i = 1;
+        let queue = [start];
+
+        while (i < levelArr.length) {
+            const curr = queue.shift();
+
+            if (levelArr[i]) {
+                curr.left = new TreeNode(levelArr[i], null, null);
+                queue.push(curr.left);
+            }
+            i += 1;
+
+            if (levelArr[i]) {
+                curr.right = new TreeNode(levelArr[i], null, null);
+                queue.push(curr.right);
+            }
+            i += 1;
+        }
+        return start;
+    }
+}
+var goodNodes = function (root) {
+    let max = root.val;
+    let goodCount = 0;
+
+    function dfs(root, max) {
+        if (root == null) return;
+
+        if (root.val >= max) {
+            goodCount += 1;
+            max = root.val;
+        }
+        dfs(root.left, max);
+        dfs(root.right, max);
+    }
+    dfs(root, max);
+
+    return goodCount;
+};
+const b = new BinaryTree();
+const tree = b.buildByLevelOrder([3, 1, 4, 3, null, 1, 5]);
+console.log(goodNodes(tree));
