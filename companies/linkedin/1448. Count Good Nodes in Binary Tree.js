eb67d3f9f812a6c90e5968a83f66cb4dc9f4aa3f
saransh-khobragade <saransh98@gmail.com>
2025-09-24 23:39:14 +0530

    linkedin one down

---
+++ companies/linkedin/1448. Count Good Nodes in Binary Tree.js
@@ -0,0 +1,8 @@
+/*
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+Return the number of good nodes in the binary tree.
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+*/
+var goodNodes = function (root) {};
