2b2d5183ddcc64c0d8960e453198b135331e93c9
saransh-khobragade <saransh98@gmail.com>
2026-01-18 09:38:17 +0530

    added java structure

---
+++ java/pattern/twoPointer/ValidParentheses.java
@@ -0,0 +1,13 @@
+package pattern.twoPointer;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        ValidParentheses obj = new ValidParentheses();
+        obj.isValid("abc");
+
+    }
+    public boolean isValid(String s) {
+        System.out.println("hello isvalid");
+        return true;
+    }
+}
