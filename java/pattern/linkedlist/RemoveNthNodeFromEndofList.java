49b6a15e5236375c61cdcad10a72382c0e849997
saransh-khobragade <saransh98@gmail.com>
2026-04-09 20:33:49 +0530

    Remove Nth Node From End of List

--- java/pattern/linkedlist/RemoveNthNodeFromEndofList.java
+++ java/pattern/linkedlist/RemoveNthNodeFromEndofList.java
@@ -5,16 +5,34 @@ import syntax.ListNode;
 
 public class RemoveNthNodeFromEndofList {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1, 2};
         ListNode head = LinkedList.build(arr);
 
-        ListNode result = removeNthFromEnd(head,2);
+        ListNode result = removeNthFromEnd(head, 2);
         LinkedList.display(result);
     }
+
     public static ListNode removeNthFromEnd(ListNode head, int n) {
 
+        if (head.next == null & n > 1) return null;
 
+        ListNode first = head;
+        ListNode last = head;
 
+        int i = 0;
+        while (i < n) {
+            last = last.next;
+            i++;
+        }
+        if(last != null){
+            while (last.next != null) {
+                first = first.next;
+                last = last.next;
+            }
+            first.next = first.next.next;
+        }else{
+            head = first.next;
+        }
         return head;
     }
 }
